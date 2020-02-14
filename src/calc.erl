91e8135091ac3b229cb1d3cd61bf62d1e7a3cd6d
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-14 19:25:03 +0100

    create basic func of calc

--- src/calc.erl
+++ src/calc.erl
@@ -1,3 +1,15 @@
 -module(calc).
 
--export([]).
+-export([add/2, sub/2, multi/2, division/2]).
+
+add(X,Y) -> 
+    X+Y.
+
+sub(X,Y) ->
+    X-Y.
+
+multi(X,Y) ->
+    X*Y.
+
+division(X,Y) ->
+    X/Y.
