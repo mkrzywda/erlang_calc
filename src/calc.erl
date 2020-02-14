3d9bc7c96a844cab00efa8ac28a8104ad7b5c36c
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-14 19:43:28 +0100

    Add @docs and fix error with div

--- src/calc.erl
+++ src/calc.erl
@@ -2,14 +2,31 @@
 
 -export([add/2, sub/2, multi/2, division/2]).
 
+
+%% @doc Adds two integers
+%%
+%% This adds `X' to `Y'.
+-spec add(integer(), integer()) -> integer().
 add(X,Y) -> 
     X+Y.
 
+%% @doc Subs two integers
+%%
+%% This subs `X' to `Y'.
+-spec sub(integer(), integer()) -> integer().
 sub(X,Y) ->
     X-Y.
 
+%% @doc Adds two integers
+%%
+%% This multi `X' by `Y'.
+-spec multi(integer(), integer()) -> integer().
 multi(X,Y) ->
     X*Y.
 
+%% @doc Div two integers
+%%
+%% This div `X' to `Y'.
+-spec division(integer(), integer()) -> integer().
 division(X,Y) ->
-    X/Y.
+    X div Y.
