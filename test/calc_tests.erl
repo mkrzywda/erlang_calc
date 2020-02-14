8915cce9ff20486b161d837173a78ca8ebe1d3ac
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-14 19:44:44 +0100

    Create unit test for calc and add cover options to config

---
+++ test/calc_tests.erl
@@ -0,0 +1,15 @@
+-module(calc_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+add_test() ->
+    ?assertEqual(2+3, calc:add(2,3)).
+
+sub_test() ->
+    ?assertEqual(2-3, calc:sub(2,3)).
+
+mul_test() ->
+    ?assertEqual(2*3, calc:multi(2,3)).
+
+div_test() ->
+    ?assertEqual(3 div 3, calc:division(3,3)).
+
