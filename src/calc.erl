38d0f15a98567eb90e6de2b19c912f3066d9f11f
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-14 19:58:38 +0100

    add power func

--- src/calc.erl
+++ src/calc.erl
@@ -30,3 +30,10 @@ multi(X,Y) ->
 -spec division(integer(), integer()) -> integer().
 division(X,Y) ->
     X div Y.
+
+%% @doc Power to intigers
+%% 
+%% This power `X` to `Y`
+-spec power(integer(), integer()) -> integer().
+power(X,Y) ->
+    X ** Y.
